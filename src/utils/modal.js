3cff647b4a1903ccd1d341324bbf129da1ea93cb
Arseniy Zakharov <a.zaharov@aristos.pw>
2022-08-21 18:06:42 +0300

    Initial commit

---
+++ src/utils/modal.js
@@ -0,0 +1,29 @@
+class Modal {
+	initModal (triggerButtonSelector) {
+		const openButton = document.querySelector(triggerButtonSelector);
+		const modal = document.querySelector('.base-modal');
+		const modalContainer = document.querySelector('.base-modal__container');
+		const modalClose = document.querySelector('.base-modal__close');
+
+		modalContainer.addEventListener('click', (e) => {
+			e.stopPropagation();
+		})
+
+		openButton.addEventListener('click', this.openModal);
+		modal.addEventListener('click', this.closeModal);
+		modalClose.addEventListener('click', this.closeModal);
+	}
+
+	openModal() {
+		const modal = document.querySelector('.base-modal');
+		modal.classList.add('base-modal--open');
+	}
+
+	closeModal() {
+		const modal = document.querySelector('.base-modal');
+		modal.classList.remove('base-modal--open');
+	}
+}
+
+export default Modal
+
